f9bbab6fb760aa8f580941c911881963e14b4865
Felix Drees <fdrees@uni-bremen.de>
2024-01-15 22:30:40 +0100

    I'm guessing this may start causing us problems either soon or never.

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
+main: d
+
 clean:
 	rm thesis-template-in-org-mode.*
 
 doom:
 	~/.config/emacs/bin/doom run &
 
-
 d: doom
+
+spell:
+	aspell -c README.org
--- shell.nix
+++ shell.nix
@@ -2,8 +2,18 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python310.withPackages(ps: with ps; [ numpy latexify-py scipy pygments ]))
+    (python310.withPackages(ps: with ps; [
+	numpy         # for math
+	latexify-py   # for converting python funcs to latex
+	scipy         # for math and physics
+	pygments      # for syntax highlighting
+	matplotlib    # for plotting
+	seaborn       # for plotting
+	scikit-learn  # for ml stuff
+    ]))
+    texlive.combined.scheme-full
     graphviz
+    gnuplot
   ];
 
   shellHook = ''
